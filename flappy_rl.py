1c58d1305c23c73a89de084bdec8d0150782f5d2
KNÜNZ Nico <nkn5729@students.fhv.at>
2024-03-25 15:24:41 +0100

    initial commit

---
+++ flappy_rl.py
@@ -0,0 +1,31 @@
+import flappy_bird_gymnasium
+import gymnasium
+import numpy as np
+
+# our environment is the environment our flappy bird plays in
+# the environment gives us our observations, our possible actions in our environment, our reward and some more info (terminated or not)
+env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+
+# get numbers of possible actions in env
+nb_actions = env.action_space.n
+
+# create a simple model
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+
+policy = EpsGreedyQPolicy()
+memory = SequentialMemory(limit=50000, window_length=1)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this slows down training quite a lot. 
+dqn.fit(env, nb_steps=5000, visualize=True, verbose=2)
+
+env.close()
